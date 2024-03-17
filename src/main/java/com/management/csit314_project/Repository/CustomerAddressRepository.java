b00624e7f1711694e0b9185b467b0b35227126e5
Elvanss <duy.elvans12@gmail.com>
2024-03-17 22:46:49 +1100

    delete repo

--- src/main/java/com/management/csit314_project/Repository/CustomerAddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.management.csit314_project.Repository;
-
-import com.management.csit314_project.Model.CustomerAddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerAddressRepository extends JpaRepository<CustomerAddress, Integer> {
-}
--- src/main/java/com/management/csit314_project/Repository/FeedBackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.management.csit314_project.Repository;
-
-import com.management.csit314_project.Model.FeedBack;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FeedBackRepository extends JpaRepository<FeedBack, Integer> {
-}
